4f2f71dc01e15ef9f5ee804e0725f7d4a4352d66
HimchanBae <oiu3056@naver.com>
2024-08-05 13:43:34 +1000

    Part3 - 0.1~0.6 Clear!

---
+++ part3/0.3/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3001;
+app.use(express.json());
+
+let phoneBook = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(phoneBook);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const entry = phoneBook.find((entry) => entry.id === id);
+
+  if (entry) {
+    response.json(entry);
+  } else {
+    response.status(404).send({ error: "Note not found" });
+  }
+});
+
+app.get("/info", (request, response) => {
+  const entryCount = phoneBook.length;
+  const currentTime = new Date();
+
+  response.send(
+    `<p>Phonebook has info for ${entryCount} people</p><p>${currentTime}</p>`
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

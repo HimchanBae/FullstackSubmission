da2ca9fe377729ea47667ecf8bd4a5307e38236f
HimchanBae <oiu3056@naver.com>
2024-08-31 00:21:05 +1000

    Part1 - 1.12 Clear

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -67,14 +67,32 @@ const Display = (props) => {
 const App = () => {
   const feedbackHeader = "give feedback";
   const statisticsHeader = "statistics";
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
   // 각 버튼의 클릭 수를 자신의 상태로 저장
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
 
   return (
     <div>
+      {anecdotes[selected]}
+      <br />
+      <Button
+        handleClick={() => setSelected(Math.floor(Math.random() * 8))}
+        text="next anecdote"
+      />
+      <Button handleClick={() => setSelected(0)} text="reset anectdote" />
       <Display header={feedbackHeader} />
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />

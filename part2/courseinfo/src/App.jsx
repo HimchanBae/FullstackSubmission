3d7b6a3510b744fcb6e52db9b52d2b5143302c38
HimchanBae <oiu3056@naver.com>
2024-08-31 16:02:55 +1000

    Part2 - 2.3 & 2.4 Clear

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,8 +1,6 @@
 import Course from "./components/Course";
 import { useState, useEffect } from "react";
 
-const Header = ({ course }) => <h1>{course}</h1>;
-
 const Part = ({ part }) => (
   <p>
     {part.name}&nbsp;
@@ -10,23 +8,32 @@ const Part = ({ part }) => (
   </p>
 );
 
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const CourseComponent = ({ course }) => (
+  <div>
+    <h2>{course.name}</h2>
+    <Content parts={course.parts} />
+    <b>
+      Total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)}{" "}
+      exercises
+    </b>
+  </div>
+);
+
 const App = () => {
-  const [sum, setSum] = useState(0);
-
-  useEffect(() => {
-    // 합계를 계산하고 상태로 설정
-    const total = Course.parts.reduce((acc, part) => acc + part.exercises, 0);
-    setSum(total);
-  }, []);
-
   return (
     <div>
-      <Header course={Course.name} />
-
-      {Course.parts.map(
-        (part) => (console.log(part), (<Part key={part.id} part={part} />))
-      )}
-      <b>Total of {sum} exercises</b>
+      <h1>Web development curriculum</h1>
+      {Course.map((course) => (
+        <CourseComponent key={course.id} course={course} />
+      ))}
     </div>
   );
 };
